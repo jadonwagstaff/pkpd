90edca32c8fcb5b04f16f0d9b7ffc3715cd11e7e
Jadon Shane Wagstaff <u0590631@ad.utah.edu>
2017-10-04 10:56:21 -0600

    Simplified clean_htwt.

--- R/clean_htwt.R
+++ R/clean_htwt.R
@@ -1,22 +1,32 @@
+# Jadon Wagstaff
+# University of Utah Health
+# Pediatrics
+# Clinical Pharmacology
+# 2017
+
 #' Height and Weight anomaly detection.
 #'
 #' Uses CDC height and weight expected values to determine anomalies
 #' in height and weight values for cleaning purposes.
 #'
 #' See \url{https://www.cdc.gov/growthcharts/percentile_data_files.htm}
-#' for data sources used to detect anomalies.
-#'
-#' Datasets are included in package as statage, lenageinf, wtage,
-#' and wtageinf.
+#' for data sources used to detect anomalies. Z-scores are calculated
+#' based on LMS parameters as described in [1]. If subject is greater
+#' than 20 years old, height and weight will be compared to the distribution
+#' for a 20 year old.
 #'
 #' If any dataframe input column has a different name, define
 #' that columns name in the input parameters (See Examples).
-#'
-#' See http://r4ds.had.co.nz/ chapters 11.4, 11.5 and 16 for creating
+#' See http://r4ds.had.co.nz chapters 11.4, 11.5 and 16 for creating
 #' date or datetime R objects.
 #'
+#' 1. Flegal KM, Cole TJ. Construction of LMS parameters for the Centers
+#' for Disease Control and prevention 2000 growth chart. National health
+#' statistics reports; no 63. Hyattsville, MD: National Center for Health
+#' Statistics. 2013.
+#'
 #' @param df Data frame containing weight, height, the time weight and
-#' height were collected, sex, and eighter birth date, or age in months.
+#' height were collected, sex, and eighter birth date or age in months.
 #'
 #' @param HEIGHT Column name for patient height (cm).
 #'
@@ -27,54 +37,36 @@
 #' @param TIME Column name for dates when corresponding height and
 #' weight values were collected. Must be R datetime object.
 #'
-#' @param BIRTH_DT Birth date of patient. Must be R date or
+#' @param BIRTH_DT Column name for birth date of patient. Must be R date or
 #' datetime object.
 #'
 #' @param AGE_M Column name for age of patient in months.
 #'
-#' @param lower_percentile Integer value representing the lower
-#' percentile, upper percentile will automatically be calculated.
-#'
-#' Options: 3,5,10,25
-#'
-#' Example: lower_percentile = 3. Normal will be defined as inbetween
-#' fith and ninety fifth percentile (3-97).
-#'
-#' lower_percentile = 5, (5-95).
-#'
-#' lower_percentile = 10, (10-90).
-#'
-#' lower_percentile = 25, (25-75).
-#'
 #' @return Returns a data frame identifying height weight anomalies
 #' with specific colums:
-#'
-#' \strong{HT_FLAG:} Numerical value. >0 signifying distance from
-#' normal if height is abnormal. 0 if there is no abnormality in
-#' height detected.
 #'
 #' \strong{HT_Z:} Z score on expected distribution of height for
 #' age and sex.
 #'
-#' \strong{WT_FLAG:} Numerical value. >0 signifying distance from
-#' normal if weight is abnormal. 0 if there is no abnormality in
-#' weight detected.
-#'
 #' \strong{WT_Z:} Z score on expected distribution of Weight for
 #' age and sex.
 #'
-#' \strong{AGE_Y:} Age of patient in years.
-#'
 #' @keywords height, weight, cleaning, clean
 #' @examples
-#' clean_htwt(htwt_example, 3, HEIGHT = HT, WEIGHT = WT)
+#' htwt <- clean_htwt(htwt_example, HEIGHT = HT, WEIGHT = WT)
+#' htwt
+#' # filter out results for inbetween 5th and 95th percentile
+#' htwt <- dplyr::filter(htwt, abs(HT_Z) > 1.96 | abs(WT_Z) > 1.96)
+#' htwt
 #' @export
-clean_htwt <- function(df, lower_percentile, HEIGHT = HEIGHT, WEIGHT = WEIGHT,
-                       SEX = SEX, TIME = TIME, BIRTH_DT = BIRTH_DT, AGE_M = AGE_M) {
+clean_htwt <- function(df, HEIGHT = HEIGHT, WEIGHT = WEIGHT, SEX = SEX,
+                       TIME = TIME, BIRTH_DT = BIRTH_DT, AGE_M = AGE_M) {
   #--------------------------------------------------------------------------------
   # ESSENTIAL FUNCTIONS
   #--------------------------------------------------------------------------------
 
+  # score
+  #
   # find z-score based on ht/wt, sex, and age group
   score <- function(x, age, set) {
     l <- splines[[set]][["L"]](age)
@@ -85,70 +77,6 @@ clean_htwt <- function(df, lower_percentile, HEIGHT = HEIGHT, WEIGHT = WEIGHT,
     output[l != 0] <- (x^l - m^l) / (m^l * l * s)
     output[l == 0] <- log(x / m) / s
     return(output)
-  }
-
-  # find_set_anomalies
-  #
-  # find anomalies for a specific sex/age group
-  # input htwt_set = set to find abnormalities
-  # input sex = 0 female, 1 male
-  # input age_set = "infant" or "child"
-  find_set_anomalies <- function(htwt_set, sex, age_set) {
-    if (nrow(htwt_set) > 0) {
-      ha <- switch(age_set,
-                   "infant" = lenageinf,
-                   "child" = statage)
-      wa <- switch(age_set,
-                   "infant" = wtageinf,
-                   "child" = wtage)
-      htwt_set$HT_FLAG <- compare_spline(x = htwt_set$AGE,
-                                         y = htwt_set$HEIGHT,
-                                         d = get_spline_data(ha, sex))
-      htwt_set$WT_FLAG <- compare_spline(x = htwt_set$AGE,
-                                         y = htwt_set$WEIGHT,
-                                         d = get_spline_data(wa, sex))
-    }
-    return(htwt_set)
-  }
-
-  # get_spline_data
-  #
-  # function to prepare information to find cubic spline
-  # input d = dataset to pull from
-  # input sex = 0 female, 1 male
-  # input x = column to use for x values
-  get_spline_data <- function(d, sex) {
-    if (sex == 0) {
-      d <- dplyr::filter(d, Sex == 2)
-    } else {
-      d <- dplyr::filter(d, Sex == 1)
-    }
-    d <- dplyr::select(d,
-                       x = Agemos,
-                       low = percentile["low"],
-                       high = percentile["high"])
-    return(d)
-  }
-
-  # compare_spline
-  #
-  # find anomalies for a specific spline set
-  # input x = vector values to use finding percentile
-  # input y = vector values to compare to find abnormality
-  # input spline_data = data frame, and spline data frame
-  #    with x, low, high values
-  compare_spline <- function(x, y, d) {
-    low_spline <- splinefun(x = d$x,
-                            y = d$low,
-                            method = "natural")
-    high_spline <- splinefun(x = d$x,
-                             y = d$high,
-                             method = "natural")
-    low <- y - low_spline(x)
-    low <- low * (low < 0)
-    high <- y - high_spline(x)
-    high <- high * (high > 0)
-    return(minmax(low, high))
   }
 
   # minmax
@@ -176,13 +104,6 @@ clean_htwt <- function(df, lower_percentile, HEIGHT = HEIGHT, WEIGHT = WEIGHT,
   BIRTH_DT <- deparse(substitute(BIRTH_DT))
   AGE_M <- deparse(substitute(AGE_M))
 
-  # prepare percentile information
-  percentile <- switch(as.character(lower_percentile),
-                       "3" = c(low = "P3", high = "P97"),
-                       "5" = c(low = "P5", high = "P95"),
-                       "10" = c(low = "P10", high = "P90"),
-                       "25" = c(low = "P25", high = "P75"))
-
   # prepare htwt tibble which will be used for calculations
   if (AGE_M %in% colnames(df)) {
     age <- df[[AGE_M]]
@@ -191,6 +112,7 @@ clean_htwt <- function(df, lower_percentile, HEIGHT = HEIGHT, WEIGHT = WEIGHT,
     birth_dt <- as.numeric(readr::parse_datetime(df[[BIRTH_DT]])) / (24 * 3600 * 30.41667)
     age <- time - birth_dt
   }
+  age[age > 240] <- 240
   htwt <- tibble::tibble(AGE = age,
                          HEIGHT = df[[HEIGHT]],
                          WEIGHT = df[[WEIGHT]],
@@ -198,33 +120,9 @@ clean_htwt <- function(df, lower_percentile, HEIGHT = HEIGHT, WEIGHT = WEIGHT,
   )
   htwt <- dplyr::mutate(htwt,
                         ROW = row_number(),
-                        HT_FLAG = 0,
-                        WT_FLAG = 0,
                         HT_Z = 0,
                         WT_Z = 0)
 
-
-  #--------------------------------------------------------------------------------
-  # FIND ANOMALIES
-  #--------------------------------------------------------------------------------
-  for (i in 0:1) {
-    # 0-36 months
-    htwt_set <- dplyr::filter(htwt, AGE <= 36 & SEX == i)
-    htwt_set <- find_set_anomalies(htwt_set, i, "infant")
-    htwt$HT_FLAG[htwt_set$ROW] <- minmax(htwt$HT_FLAG[htwt_set$ROW], htwt_set$HT_FLAG)
-    htwt$WT_FLAG[htwt_set$ROW] <- minmax(htwt$WT_FLAG[htwt_set$ROW], htwt_set$WT_FLAG)
-
-    # 24-240 months
-    htwt_set <- dplyr::filter(htwt, AGE > 24 & SEX == i)
-    htwt_set <- find_set_anomalies(htwt_set, i, "child")
-    htwt$HT_FLAG[htwt_set$ROW] <- minmax(htwt$HT_FLAG[htwt_set$ROW], htwt_set$HT_FLAG)
-    htwt$WT_FLAG[htwt_set$ROW] <- minmax(htwt$WT_FLAG[htwt_set$ROW], htwt_set$WT_FLAG)
-  }
-
-  # > 240 months
-  htwt_set <- dplyr::filter(htwt, AGE > 240)
-  htwt$HT_FLAG[htwt_set$ROW] <- NA
-  htwt$WT_FLAG[htwt_set$ROW] <- NA
 
   #--------------------------------------------------------------------------------
   # FIND ANOMALIES (Z-SCORES)
@@ -260,12 +158,13 @@ clean_htwt <- function(df, lower_percentile, HEIGHT = HEIGHT, WEIGHT = WEIGHT,
   #--------------------------------------------------------------------------------
   # GENERATE OUTPUT
   #--------------------------------------------------------------------------------
+  htwt$HT_Z[is.na(htwt$AGE)] <- NA
+  htwt$HT_Z[is.na(htwt$SEX)] <- NA
+  htwt$WT_Z[is.na(htwt$AGE)] <- NA
+  htwt$WT_Z[is.na(htwt$SEX)] <- NA
   df <- dplyr::mutate(df,
-                      HT_FLAG = htwt$HT_FLAG,
                       HT_Z = htwt$HT_Z,
-                      WT_FLAG = htwt$WT_FLAG,
-                      WT_Z = htwt$WT_Z,
-                      AGE_Y = htwt$AGE / 12)
+                      WT_Z = htwt$WT_Z)
 
   return(df)
 
